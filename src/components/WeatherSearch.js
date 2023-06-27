e35acf0120f96edd582c4099c68bdfd428fa0b5b
Satyam <nagarsatyam1995@gmail.com>
2023-06-27 19:12:01 +0530

    v1.1

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -6,6 +6,12 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function WeatherSearch(props) {
+
+    const handleSearch = (value) => {
+        if (value) {
+            props.setLocationQuery(value)
+        }
+    };
     const handleKeyPress = (event) => {
         if (event.key === 'Enter') {
             props.setLocationQuery(event.target.value)
@@ -18,16 +24,18 @@ export default function WeatherSearch(props) {
             component="form"
             onSubmit={(evt) => {
                 evt.preventDefault();
+                handleSearch(document.getElementById('submit-data').value)
             }}
             sx={{ background: 'transparent', boxShadow: 'none', display: 'flex', alignItems: 'center', minWidth: 200 }}
         >
-            <IconButton type="button" aria-label="search">
+            <IconButton type="submit" aria-label="search">
                 <SearchIcon />
             </IconButton>
 
             <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
 
             <InputBase
+                id='submit-data'
                 sx={{ ml: 1, flex: 1 }}
                 placeholder="City / Zip Code"
                 inputProps={{ 'aria-label': 'search city' }}
